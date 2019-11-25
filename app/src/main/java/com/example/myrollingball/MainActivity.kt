efa661d54c996dd57e01ebdfc5245721586a78f0
niwatoriaki <1801001@s.asojuku.ac.jp>
2019-11-25 19:46:12 +0900

    commit

--- app/src/main/java/com/example/myrollingball/MainActivity.kt
+++ app/src/main/java/com/example/myrollingball/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : AppCompatActivity(),SensorEventListener,SurfaceHolder.Callb
     private var vy:Float = 0f
     private var time:Long = 0L
     private var flag = true
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,10 +34,8 @@ class MainActivity : AppCompatActivity(),SensorEventListener,SurfaceHolder.Callb
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         button.setOnClickListener{reset()}
     }
-
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
     }
-
     override fun onSensorChanged(event: SensorEvent?) {
         if(event==null){
             return
@@ -52,41 +49,37 @@ class MainActivity : AppCompatActivity(),SensorEventListener,SurfaceHolder.Callb
             var t = (System.currentTimeMillis() - time).toFloat()
             time = System.currentTimeMillis()
             t /= 1000.0f
-
-            val dx = (vx*t) + (x*t*t)/2.0f
-            val dy = (vy*t) + (y*t*t)/2.0f
-            ballX += (dx*coef)
-            ballY += (dy*coef)
-            vx +=(x*t)
-            vy +=(y*t)
-            if( (ballX -radius)<0 && vx<0 ){
-                // 左にぶつかった時
-                vx = -vx /1.5f;
-                ballX = radius;
-            }else if( (ballX+radius)>surfaceWidth && vx>0){
-                // 右にぶつかった時
-                vx = -vx/1.5f;
-                ballX = (surfaceWidth-radius);
+            if(flag){
+                val dx = (vx*t) + (x*t*t)/2.0f
+                val dy = (vy*t) + (y*t*t)/2.0f
+                ballX += (dx*coef)
+                ballY += (dy*coef)
+                vx +=(x*t)
+                vy +=(y*t)
+                if( (ballX -radius)<0 && vx<0 ){
+                    // 左にぶつかった時
+                    vx = -vx /1.5f;
+                    ballX = radius;
+                }else if( (ballX+radius)>surfaceWidth && vx>0){
+                    // 右にぶつかった時
+                    vx = -vx/1.5f;
+                    ballX = (surfaceWidth-radius);
+                }
+                // 上下について
+                if( (ballY -radius)<0 && vy<0 ){
+                    // 下にぶつかった時
+                    vy = -vy /1.5f;
+                    ballY = radius;
+                }else if( (ballY+radius)>surfaceHeight && vy>0 ){
+                    // 上にぶつかった時
+                    vy = -vy/1.5f;
+                    ballY = surfaceHeight -radius;
+                }
+                if(ballY==ballX&&vx>0&&vy>0){
+                    vx=-vx/1.5f
+                    vy=-vy/1.5f
+                }
             }
-            // 上下について
-            if( (ballY -radius)<0 && vy<0 ){
-                // 下にぶつかった時
-                vy = -vy /1.5f;
-                ballY = radius;
-            }else if( (ballY+radius)>surfaceHeight && vy>0 ){
-                // 上にぶつかった時
-                vy = -vy/1.5f;
-                ballY = surfaceHeight -radius;
-            }
-            if(ballY==ballX&&vx>0&&vy>0){
-                vx=-vx/1.5f
-                vy=-vy/1.5f
-            }
-            if(200f<ballX&&ballX<600f&&500f<ballY&&ballY<600f){
-                flag = false
-                image.setImageResource(R.drawable.businessman4_cry)
-            }
-
             // キャンバスに描画
             this.drawCanvas();
         }
@@ -95,8 +88,6 @@ class MainActivity : AppCompatActivity(),SensorEventListener,SurfaceHolder.Callb
     override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
         surfaceWidth = width
         surfaceHeight = height
-        ballX = (width/2).toFloat()
-        ballY = (width/2).toFloat()
     }
 
     override fun surfaceDestroyed(p0: SurfaceHolder?) {
@@ -113,10 +104,25 @@ class MainActivity : AppCompatActivity(),SensorEventListener,SurfaceHolder.Callb
             SensorManager.SENSOR_DELAY_GAME
         )
     }
+
+    override fun onPause() {
+        super.onPause()
+        val accSensor = this.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accSensor.unregisterListener(this)
+    }
+
+
     private fun drawCanvas(){
         val canvas = surfaceView.holder.lockCanvas()
         canvas.drawColor(Color.DKGRAY)
-        canvas.drawRect(200f,500f,600f,600f,Paint(Color.GREEN))
+        canvas.drawRect(200f,500f,600f,600f,Paint().apply { color = Color.BLACK })
+        fall(200f,500f,600f,600f)
+        canvas.drawRect(500f,900f,600f,1200f,Paint().apply { color = Color.BLACK })
+        fall(500f,900f,600f,1200f)
+        canvas.drawRect(0f,1000f,300f,1400f,Paint().apply { color = Color.BLACK })
+        fall(0f,1000f,300f,1400f)
+        canvas.drawRect(400f,600f,500f,700f,Paint().apply { color = Color.YELLOW })
+        clear(400f,600f,500f,700f)
         if(flag){
             canvas.drawCircle(
                 ballX,
@@ -127,7 +133,6 @@ class MainActivity : AppCompatActivity(),SensorEventListener,SurfaceHolder.Callb
                 }
             )
         }
-
         surfaceView.holder.unlockCanvasAndPost(canvas)
     }
     private fun reset(){
@@ -137,6 +142,22 @@ class MainActivity : AppCompatActivity(),SensorEventListener,SurfaceHolder.Callb
         vy = 0f
         time = 0L
         flag = true
+        image.setImageResource(R.drawable.pose_ganbarou_man)
+        text.setText(R.string.ganbare)
+    }
+    private fun fall(left:Float,top:Float,right:Float,bottom:Float){
+        if(left<ballX&&ballX<right&&top<ballY&&ballY<bottom){
+            flag = false
+            image.setImageResource(R.drawable.businessman4_cry)
+            text.setText(R.string.zannnenn)
+        }
+    }
+    private fun clear(left:Float,top:Float,right:Float,bottom:Float){
+        if(left<ballX&&ballX<right&&top<ballY&&ballY<bottom){
+            image.setImageResource(R.drawable.clear)
+            flag=false
+            text.setText(R.string.omedetou)
+        }
     }
 
 }
